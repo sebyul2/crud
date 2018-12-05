094bd617b8b08c3adbc2126737dfc32875d67c06
xiu <conceptcube.tv@gmail.com>
2018-12-05 21:22:50 +0900

    model update

--- index.js
+++ index.js
@@ -7,15 +7,19 @@ const config = require('config')
 const bodyParser = require('body-parser')
 const mongooseTimestamp = require('mongoose-timestamp')
 const userModel = require('./model/user-model')
+const customerModel = require('./model/customer-model')
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // api
+//===================  user  =========================
 app.put('/user', wrap(async (req, res) => {
     // req : request(요청) 클라이언트가 보내는 요청 데이터
 
+    // 중복체크하는 로직은 향후 업데이트 해야함!!!!!!!!!!!!!!!!!
+    // 에러처리하는 부분도 향후 업데이트 해야함!!!!!!!!!!!!!!!!!
     // const name = req.body.name
     // const email = req.body.email
     // const phone = req.body.phone
@@ -58,9 +62,50 @@ app.delete('/user', wrap(async (req, res) => {
     res.json(user)
 }))
 
-app.use('/', wrap(async (req, res) => {
-    res.send('hello world')
+//=================  customer  =======================
+app.put('/customer', wrap(async (req, res) => {
+    const { company, name, email, phone } = req.body
+    const data = {
+        name, email, phone, company
+    }
+    const customer = await customerModel.create(data)
+    res.json(customer)
 }))
+
+app.get('/customer', wrap(async (req, res) => {
+    const customer = await customerModel.find()
+    res.json(customer)
+}))
+
+app.post('/customer', wrap(async (req, res) => {
+    const { id, name, email, phone, company } = req.body
+    let data = {}
+    if (name) data.name = name
+    if (email) data.email = email
+    if (phone) data.phone = phone
+    if (company) data.company = company 
+    // findOneAndUpdate 형식
+    // findOneAndUpdate(검색조건, 수정할 data, 옵션)
+    const customer = await customerModel.findOneAndUpdate(
+        { _id: id },
+        data,
+        { upsert: true, new: true }
+    )
+    res.json(customer)
+}))
+
+app.delete('/customer', wrap(async (req, res) => {
+    const { id } = req.query
+    const customer = await customerModel.deleteOne({ _id: id })
+    // _id : mongoDB의 자동 생성 아이디
+    // id : 사용자가 클라이언트에서 입력한 값 테스트시 id 사용
+    res.json(customer)
+}))
+
+//테스트용
+// app.use('/', wrap(async (req, res) => {
+//     res.send('hello world')
+// }))
 
 // monngoDB 접속 & server listening 
 mongoose.Promise = global.Promise
