261cd12e7cbd50f1c83c53fe10aa0226ae742636
sebyul2 <sebyul3@gmail.com>
2018-12-05 18:55:51 +0900

    mongoDB 접속 로직 추가, 모듈 추가

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
+'use strict'
+
 const express = require('express')
+const wrap = require('express-async-wrap')
+const mongoose = require('mongoose')
+const config = require('config')
+const mongooseTimestamp = require('mongoose-timestamp')
 const app = express()
 
-app.use('/', (req, res) => {
+// api
+app.use('/', wrap(async (req, res) => {
     res.send('hello world')
+}))
+
+// monngoDB 접속 & server listening 
+mongoose.Promise = global.Promise
+mongoose.plugin(mongooseTimestamp, {
+  createdAt: 'created_at',
+  updatedAt: 'updated_at',
 })
 
-const server = app.listen(8080, () => {
-    console.log('Express server listening on port ' + server.address().port)
+const { user, pass, host, name } = config.db
+const url = `mongodb://${user}:${pass}@${host}/${name}?authSource=admin`
+const options = { useNewUrlParser: true }
+
+mongoose.connect(url, options).then(client => {
+    console.log('mongoDB connection successful')
+    const server = app.listen(8080, () => {
+        console.log('Express server listening on port ' + server.address().port)
+    })
 })
