ef0e50e5df0d8eab35bf699e69031a31f01ec73d
sebyul2 <sebyul3@gmail.com>
2018-12-05 20:40:54 +0900

    delete, update 작성

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // api
-app.use('/', wrap(async (req, res) => {
-    res.send('hello world')
-}))
-
 app.put('/user', wrap(async (req, res) => {
     // req : request(요청) 클라이언트가 보내는 요청 데이터
 
@@ -25,7 +21,6 @@ app.put('/user', wrap(async (req, res) => {
     // const phone = req.body.phone
     // const password = req.body.password
     const { name, email, phone, password } = req.body
-
     const data = {
         name, email, phone, password
     }
@@ -34,15 +29,37 @@ app.put('/user', wrap(async (req, res) => {
 }))
 
 app.get('/user', wrap(async (req, res) => {
-    
+    const user = await userModel.find()
+    res.json(user)
 }))
 
 app.post('/user', wrap(async (req, res) => {
-    
+    const { id, name, email, phone, password } = req.body
+    let data = {}
+    if (name) data.name = name
+    if (email) data.email = email
+    if (phone) data.phone = phone
+    if (password) data.password = passowrd 
+    // findOneAndUpdate 형식
+    // findOneAndUpdate(검색조건, 수정할 data, 옵션)
+    const user = await userModel.findOneAndUpdate(
+        { _id: id },
+        data,
+        { upsert: true, new: true }
+    )
+    res.json(user)
 }))
 
 app.delete('/user', wrap(async (req, res) => {
-    
+    const { id } = req.query
+    const user = await userModel.deleteOne({ _id: id })
+    // _id : mongoDB의 자동 생성 아이디
+    // id : 사용자가 클라이언트에서 입력한 값 테스트시 id 사용
+    res.json(user)
+}))
+
+app.use('/', wrap(async (req, res) => {
+    res.send('hello world')
 }))
 
 // monngoDB 접속 & server listening 
